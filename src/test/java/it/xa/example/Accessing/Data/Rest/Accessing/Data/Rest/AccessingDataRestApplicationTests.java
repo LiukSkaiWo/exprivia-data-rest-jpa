a43dc499068ff0aa6e4cf3774a9c6e219e9c9e5d
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-08-22 11:53:10 +0200

    Added all the spring initializr files before modifying it

---
+++ src/test/java/it/xa/example/Accessing/Data/Rest/Accessing/Data/Rest/AccessingDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.xa.example.Accessing.Data.Rest.Accessing.Data.Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
